657830337237111737bbf3a23f8d15f5abc50179
Eduardo Maia <maia@eduardomaia.com>
2014-03-09 19:40:13 -0700

    Creating repository and first commit

---
+++ Clone/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BottomToolStripPanel = New System.Windows.Forms.ToolStripPanel()
+        Me.TopToolStripPanel = New System.Windows.Forms.ToolStripPanel()
+        Me.RightToolStripPanel = New System.Windows.Forms.ToolStripPanel()
+        Me.LeftToolStripPanel = New System.Windows.Forms.ToolStripPanel()
+        Me.ContentPanel = New System.Windows.Forms.ToolStripContentPanel()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files|*"
+        '
+        'BottomToolStripPanel
+        '
+        Me.BottomToolStripPanel.Location = New System.Drawing.Point(0, 0)
+        Me.BottomToolStripPanel.Name = "BottomToolStripPanel"
+        Me.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.BottomToolStripPanel.RowMargin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.BottomToolStripPanel.Size = New System.Drawing.Size(0, 0)
+        '
+        'TopToolStripPanel
+        '
+        Me.TopToolStripPanel.Location = New System.Drawing.Point(0, 0)
+        Me.TopToolStripPanel.Name = "TopToolStripPanel"
+        Me.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.TopToolStripPanel.RowMargin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.TopToolStripPanel.Size = New System.Drawing.Size(0, 0)
+        '
+        'RightToolStripPanel
+        '
+        Me.RightToolStripPanel.Location = New System.Drawing.Point(0, 0)
+        Me.RightToolStripPanel.Name = "RightToolStripPanel"
+        Me.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.RightToolStripPanel.RowMargin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.RightToolStripPanel.Size = New System.Drawing.Size(0, 0)
+        '
+        'LeftToolStripPanel
+        '
+        Me.LeftToolStripPanel.Location = New System.Drawing.Point(0, 0)
+        Me.LeftToolStripPanel.Name = "LeftToolStripPanel"
+        Me.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.LeftToolStripPanel.RowMargin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.LeftToolStripPanel.Size = New System.Drawing.Size(0, 0)
+        '
+        'ContentPanel
+        '
+        Me.ContentPanel.AutoScroll = True
+        Me.ContentPanel.Size = New System.Drawing.Size(605, 234)
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Location = New System.Drawing.Point(2, 94)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(578, 85)
+        Me.GroupBox3.TabIndex = 18
+        Me.GroupBox3.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
+        Me.Button4.Location = New System.Drawing.Point(492, 16)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 58)
+        Me.Button4.TabIndex = 11
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox3.Location = New System.Drawing.Point(9, 54)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(468, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(287, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "The directory or hard drive where your back up will be done"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(6, 16)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(126, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Destination Directory"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(2, 3)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(578, 85)
+        Me.GroupBox2.TabIndex = 17
+        Me.GroupBox2.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.Location = New System.Drawing.Point(492, 16)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 58)
+        Me.Button3.TabIndex = 11
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(9, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(468, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 33)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(261, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Where are located your files that you need to back up"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Source Directory"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(462, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 23)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Backup my files"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 225)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 20
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(8, 199)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(324, 17)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Remove files from destination that don't exist in source directory"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 238)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Maia Cloneme 1.0"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents BottomToolStripPanel As System.Windows.Forms.ToolStripPanel
+    Friend WithEvents TopToolStripPanel As System.Windows.Forms.ToolStripPanel
+    Friend WithEvents RightToolStripPanel As System.Windows.Forms.ToolStripPanel
+    Friend WithEvents LeftToolStripPanel As System.Windows.Forms.ToolStripPanel
+    Friend WithEvents ContentPanel As System.Windows.Forms.ToolStripContentPanel
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Clone/Form1.vb
@@ -0,0 +1,244 @@
+﻿Imports System
+Imports System.IO
+Imports System.Collections
+Imports System.Collections.Specialized
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Dim CommandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String) = My.Application.CommandLineArgs
+
+    Private Function ValidateForm() As Boolean
+        Dim returnValue As Boolean
+        returnValue = True
+
+        If (TextBox2.Text = "") Then
+            TextBox2.BackColor = Color.Red
+            returnValue = False
+        End If
+        If (TextBox3.Text = "") Then
+            TextBox3.BackColor = Color.Red
+            returnValue = False
+        End If
+
+        Return returnValue
+    End Function
+
+
+    Private Function GetModifiedDate(ByVal filepath As String) As Date
+        Return System.IO.File.GetLastWriteTime(filepath) '.ToString("yyyy-mm-dd HH:mm:ss")
+    End Function
+
+
+    ' LOGAR TEXTO EM ARQUIVO
+    Private Sub loga(ByVal msg As String)
+        Using sw As StreamWriter = File.AppendText("clone.log")
+            sw.WriteLine(msg)
+        End Using
+    End Sub
+
+    Private Function GetFileSize(ByVal filepath As String)
+        If (Not System.IO.File.Exists(filepath)) Then
+            Return 0
+        Else
+            Dim a As IO.FileInfo = New IO.FileInfo(filepath)
+            Return a.Length
+        End If
+    End Function
+
+    Private Sub ListAll(ByVal Directory As String, ByVal newdir As StringCollection, ByVal files As StringCollection)
+        Try
+
+            ' LIST FILES
+            Dim di As New IO.DirectoryInfo(Directory)
+            Dim diar1 As IO.FileInfo() = di.GetFiles()
+            Dim dra As IO.FileInfo
+            For Each dra In diar1
+                files.Add(Directory & "\" & dra.ToString())
+                Application.DoEvents()
+            Next
+
+            ' LIST DIRECTORIES
+            Dim di2 = New IO.DirectoryInfo(Directory)
+            Dim diar2 As IO.DirectoryInfo() = di2.GetDirectories()
+            Dim dra2 As IO.DirectoryInfo
+            For Each dra2 In diar2
+                newdir.Add(Directory & "\" & dra2.ToString)
+                Application.DoEvents()
+            Next
+        Catch ex As Exception
+            loga(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim config_ini_path As String
+        config_ini_path = IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) + "\config.ini"
+        config_ini_path = config_ini_path.Replace("file:\", "")
+
+        If (System.IO.File.Exists(config_ini_path)) Then
+            Using reader As StreamReader = New StreamReader(config_ini_path)
+                TextBox2.Text = reader.ReadLine
+                TextBox3.Text = reader.ReadLine
+            End Using
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Process.Start("http://www.eduardomaia.com/cgi-bin/redirect.pl?redirect=maiaclone")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim files As New StringCollection
+        Dim newdir As New StringCollection
+
+        'SAVE TEXTBOXES TO A TEXT FILE
+        Dim objWriter As New System.IO.StreamWriter(Directory.GetCurrentDirectory() + "\config.ini")
+        objWriter.WriteLine(TextBox2.Text)
+        objWriter.WriteLine(TextBox3.Text)
+        objWriter.Close()
+
+        files.Clear()
+        newdir.Clear()
+
+        If (Not ValidateForm()) Then
+            Exit Sub
+        End If
+
+        Label1.Text = "Searching for files to backup..."
+
+        'DISABLE ALL FORM ELEMENTS
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        Button1.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+
+        loga("---------------------------------------------")
+        loga("Backup session start at " & DateTime.Now.ToString("dddd, dd MMMM yyyy hh:mm:ss "))
+        loga("Getting files and directories from source " & TextBox2.Text & "...")
+        newdir.Add(TextBox2.Text)
+
+        While (newdir.Count > 0)
+            ListAll(newdir(0), newdir, files)
+            newdir.RemoveAt(0)
+        End While
+        loga("Found a total of " & files.Count & " files.")
+
+        Label1.Text = "Found a total of " & files.Count & " files. Copying your files..."
+
+        ' SEARCHING FILES TO BE COPIED TO BACK UP DIRECTORY
+        loga("Searching files to be copied to the backup directory")
+        For Each File As String In files
+            Application.DoEvents()
+            Dim novo_destino As String = File.Replace(TextBox2.Text, TextBox3.Text)
+            If (System.IO.File.Exists(novo_destino) And GetModifiedDate(File) > GetModifiedDate(novo_destino) And GetFileSize(File) > GetFileSize(novo_destino)) Then
+                loga("IMPORTANT WARNING, POSSIBLE DISK ERROR: " & File & " AND " & novo_destino & " HAVE THE SAME MODIFIED DATE, BUT SIZES OF THESE FILES ARE DIFFERENT.")
+            End If
+            If (Not (System.IO.File.Exists(novo_destino)) Or GetModifiedDate(File) > GetModifiedDate(novo_destino) Or GetFileSize(File) > GetFileSize(novo_destino)) Then
+                loga(File & " => " & novo_destino)
+                ' BEGIN CHECK MKDIR
+                Dim dirs As String() = novo_destino.Split(New Char() {"\"c})
+                Dim full_mkdir_path As String = ""
+                Dim i As Integer
+                For i = 0 To dirs.Length - 2
+                    full_mkdir_path = full_mkdir_path & dirs(i)
+                    If (Not System.IO.Directory.Exists(full_mkdir_path)) Then
+                        System.IO.Directory.CreateDirectory(full_mkdir_path)
+                    End If
+                    full_mkdir_path = full_mkdir_path & "\"
+                    Application.DoEvents()
+                Next
+                ' END MKDIR CHECK
+
+                System.IO.File.Copy(File, novo_destino, True)
+            End If
+        Next
+
+
+        Dim files_destino As New StringCollection
+        Dim dir_destino As New StringCollection
+        Dim TempDir As String
+
+        'REMOVENDO EM DESTINO OS ARQUIVOS QUE FORAM REMOVIDOS DA ORIGEM
+        Label1.Text = "Removing files from backup directory that were removed from source directory..."
+        If (CheckBox1.Checked) Then
+
+            dir_destino.Add(TextBox3.Text)
+
+            While (dir_destino.Count > 0)
+                Application.DoEvents()
+                ListAll(dir_destino(0), dir_destino, files_destino)
+                TempDir = dir_destino(0)
+                TempDir = TempDir.Replace(TextBox3.Text, TextBox2.Text)
+                If (Not System.IO.Directory.Exists(TempDir)) Then
+                    TempDir = TempDir.Replace(TextBox2.Text, TextBox3.Text)
+                    If (System.IO.Directory.Exists(TempDir)) Then
+                        Try
+                            System.IO.Directory.Delete(TempDir, "true")
+                        Catch ex As Exception
+                            loga(ex.Message)
+                        End Try
+                    End If
+                End If
+
+                dir_destino.RemoveAt(0)
+            End While
+            For i = 0 To files_destino.Count - 1
+                Application.DoEvents()
+                files_destino(i) = files_destino(i).Replace(TextBox3.Text, TextBox2.Text)
+                If (Not files.Contains(files_destino(i))) Then
+                    files_destino(i) = files_destino(i).Replace(TextBox2.Text, TextBox3.Text)
+                    loga("Removed " & files_destino(i))
+                    If (System.IO.File.Exists(files_destino(i))) Then
+                        System.IO.File.Delete(files_destino(i))
+                    End If
+                End If
+            Next
+
+        End If
+
+        'TODO: CHECANDO SE EXISTE ARQUIVOS NA ORIGEM QUE NÃO ESTÃO NO DESTINO
+        'this is just a double check
+
+        'ENABLE ALL FORM ELEMENTS
+        TextBox2.Enabled = True
+        TextBox3.Enabled = True
+        Button1.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+
+        Label1.Text = "Done."
+
+        files.Clear()
+        newdir.Clear()
+        files_destino.Clear()
+        dir_destino.Clear()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If FolderBrowserDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If FolderBrowserDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            TextBox3.Text = FolderBrowserDialog1.SelectedPath
+        End If
+
+    End Sub
+
+    Private Sub Form1_Shown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Shown
+        For i As Integer = 0 To CommandLineArgs.Count - 1
+            If (CommandLineArgs(i) = "/go") Then
+                Application.DoEvents()
+                Button1_Click(Nothing, Nothing)
+            End If
+        Next
+
+    End Sub
+End Class
